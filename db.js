b93ab3ff0a990a070534c29239a37ecd9c24ff00
itsfarhankhan28 <famannat@gmail.com>
2023-08-13 20:39:53 +0530

    refactor(db): changed the database configuration

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const POOL = require('pg').Pool
 
 const pool = new POOL({
     user:"farhanapi",
-    password:"farhankhan",
-    host:"localhost",
+    password:"0IH3a5zBak3uy592MpU89fle1UhpiCeJ",
+    host:"dpg-cjccs77db61s73dvam9g-a",
     port:5432,
-    database:"pgserver"
+    database:"pgserver_w352"
 })
 
 module.exports = pool
